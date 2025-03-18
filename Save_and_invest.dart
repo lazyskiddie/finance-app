e1064522e1c46963f89b7c5afac46317cc852527
Sandeep Singh <sandeepsingh08550@gmail.com>
2025-03-19 02:23:31 +0530

    Update Save_and_invest.dart

--- Save_and_invest.dart
+++ Save_and_invest.dart
@@ -1,76 +1,169 @@
+import 'package:financetalk07/Accounts.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: SaveInvestPage(),
-  ));
+  runApp(SaveInvestApp());
+}
+
+class SaveInvestApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SaveInvestPage(),
+    );
+  }
 }
 
 class SaveInvestPage extends StatelessWidget {
-  final List<Map<String, dynamic>> options = [
-    {'icon': Icons.show_chart, 'label': 'Stock Market', 'image': 'assets/stock_market.png'},
-    {'icon': Icons.currency_bitcoin, 'label': 'Crypto Currency', 'image': 'assets/crypto.png'},
-    {'icon': Icons.monetization_on, 'label': 'Mutual Funds', 'image': 'assets/mutual_funds.png'},
-    {'icon': Icons.account_balance, 'label': 'FD Account', 'image': 'assets/fd_account.png'},
-    {'icon': Icons.business, 'label': 'Corporate Bonds', 'image': 'assets/corporate_bonds.png'},
-    {'icon': Icons.account_balance_wallet, 'label': 'IPO', 'image': 'assets/ipo.png'},
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.grey[300],
-        title: Text(
-          'Save & Invest',
-          style: TextStyle(color: Colors.black),
-        ),
-        automaticallyImplyLeading: false,
-        elevation: 0,
+        title: const Text('Save & Invest'),
+        backgroundColor: Colors.grey[400],
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20,
-            childAspectRatio: 1,
-          ),
-          itemCount: options.length,
-          itemBuilder: (context, index) {
-            return GestureDetector(
+        padding: const EdgeInsets.all(20.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+          children: [
+            InvestmentOption(
+              iconPath: 'assets/stock_market.png',
+              label: 'Stock Market',
               onTap: () {
-                // You can handle onTap to navigate or show details
+                Navigator.push(context, MaterialPageRoute(builder: (_) => StockMarketPage()));
               },
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      options[index]['image'],
-                      height: 50,
-                      width: 50,
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      options[index]['label'],
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          },
+            ),
+            InvestmentOption(
+              iconPath: 'assets/crypto_currency.png',
+              label: 'Crypto Currency',
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (_) => CryptoCurrencyPage()));
+              },
+            ),
+            InvestmentOption(
+              iconPath: 'assets/mutual_funds.png',
+              label: 'Mutual Funds',
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (_) => MutualFundsPage()));
+              },
+            ),
+            InvestmentOption(
+              iconPath: 'assets/fd_account.png',
+              label: 'FD Account',
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (_) => BankAccountHome()));
+              },
+            ),
+            InvestmentOption(
+              iconPath: 'assets/corporate_bonds.png',
+              label: 'Corporate Bonds',
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (_) => CorporateBondsPage()));
+              },
+            ),
+            InvestmentOption(
+              iconPath: 'assets/ipo.png',
+              label: 'IPO',
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (_) => IPOPage()));
+              },
+            ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class InvestmentOption extends StatelessWidget {
+  final String iconPath;
+  final String label;
+  final VoidCallback onTap;
+
+  InvestmentOption({required this.iconPath, required this.label, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(iconPath, height: 50),
+            const SizedBox(height: 10),
+            Text(label, style: TextStyle(fontSize: 16)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class StockMarketPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Stock Market')),
+      body: const Center(child: Text('Stock Market Information')),
+    );
+  }
+}
+
+class CryptoCurrencyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Crypto Currency')),
+      body: const Center(child: Text('Crypto Currency Information')),
+    );
+  }
+}
+
+class MutualFundsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mutual Funds')),
+      body: const Center(child: Text('Mutual Funds Information')),
+    );
+  }
+}
+
+class FDAccountPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('FD Account')),
+      body: const Center(child: Text('Account')),
+    );
+  }
+}
+
+class CorporateBondsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Corporate Bonds')),
+      body: const Center(child: Text('Corporate Bonds Information')),
+    );
+  }
+}
+
+class IPOPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('IPO')),
+      body: const Center(child: Text('IPO Information')),
     );
   }
 }
